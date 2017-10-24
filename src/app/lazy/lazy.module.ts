6e9deedbff89ec86c907160632c59e5050e50850
James Daniels <jamesdaniels@google.com>
2017-10-23 17:07:14 -0700

    Adding view count

--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -2,15 +2,24 @@ import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth'
+import { AngularFireDatabase } from 'angularfire2/database';
+
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'lazy-view',
   template: `
     <div *ngIf="article$ | async; let article; else loading">
       <h3>{{ article.title }}</h3>
+      <p>
+        <span *ngIf="viewCount$ | async; let viewCount; else loading">
+          Viewers {{ viewCount }}
+        </span>
+      </p>
       <p>
         <span *ngIf="article.author | async; let author; else loading">
           By {{ author.name }}
@@ -23,17 +32,43 @@ import { Observable } from 'rxjs/Observable';
 })
 export class LazyComponent {
   public article$: Observable<any>;
+  public viewCount$: Observable<any>;
+  public visitorRef: firebase.database.Reference | null;
 
-  constructor(db: AngularFirestore, route: ActivatedRoute) {
+  constructor(afs: AngularFirestore, rtdb: AngularFireDatabase, route: ActivatedRoute, afAuth: AngularFireAuth) {
     this.article$ = route.params.switchMap(params => 
-      db.doc(`articles/${params['id']}`).valueChanges().map(article => {
+      afs.doc(`articles/${params['id']}`).valueChanges().map(article => {
         if (article) {
           // TODO do this automatically in AngularFire
-          article['author'] = db.doc(article['author'].path).valueChanges();
+          article['author'] = afs.doc(article['author'].path).valueChanges();
         }
         return article;
       })
     );
+    this.viewCount$ = route.params.switchMap(params => 
+      rtdb.object(`articleViewCount/${params['id']}`).valueChanges()
+    )
+    Observable.combineLatest(route.params, afAuth.authState).subscribe(([params, authState]) => {
+      if (authState) {
+        if (this.visitorRef) {
+          this.visitorRef.remove();
+        }
+        this.visitorRef = rtdb.database.ref(`articleVisitors/${params['id']}/${authState.uid}`);
+        this.visitorRef.onDisconnect().remove();
+        return Observable.fromPromise(this.visitorRef.set(true));
+      } else {
+        if (this.visitorRef) {
+          this.visitorRef.remove();
+        }
+        return Observable.fromPromise(afAuth.auth.signInAnonymously());
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.visitorRef) {
+      this.visitorRef.remove();
+    }
   }
 }
 
