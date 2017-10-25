3e951b41bf3917e3c87d6949502afc547b1c2765
James Daniels <jamesdaniels@google.com>
2017-10-24 21:37:33 -0700

    Preload headers, for faster asset parsing

--- angular/firebase.ts
+++ angular/firebase.ts
@@ -2,6 +2,7 @@ import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 import { enableProdMode } from '@angular/core';
 import * as express from 'express';
+import { readFileSync } from 'fs';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
@@ -11,6 +12,14 @@ const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/mai
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
+
+
+const template = readFileSync('./dist/browser/index.html', 'utf8').toString();
+const assetsToPreload = template.match(/([^"]+\.bundle\.(js|css))/g);
+const linkHeader = assetsToPreload.map(asset => {
+    const as = asset.split('.').pop() == 'js' ? 'script' : 'style';
+    return `</${asset}>;rel=preload;as=${as};nopush`;
+}).join(',');
 
 export const app = express();
 
@@ -26,5 +35,6 @@ app.set('views', './dist/browser');
 
 app.get('**', (req, res) => {
     res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
+    res.set('Link', linkHeader);
     res.render('index', { req });
 });
