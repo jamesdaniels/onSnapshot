28ce3773a433dcfacbc07a16ee2bc3680c0a4870
James Daniels <jamesdaniels@google.com>
2019-05-31 23:06:00 +0300

    Adding first-input-delay polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -47,7 +47,7 @@ import 'core-js/es7/reflect';
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
+import 'first-input-delay';
 
 
 /***************************************************************************************************
